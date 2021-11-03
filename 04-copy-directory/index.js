45078c2ce5e27c65ff6a97509dcb8d7ab9c5f91c
valeria310 <valeriua.m@yandex.ru>
2021-11-03 17:26:40 +0300

    feat: complete task 4

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,40 @@
+const path = require('path');
+const fs = require('fs');
+const { clear } = require('console');
+const copyFolder = path.join(__dirname, 'files-copy');
+fs.mkdir(copyFolder, { recursive: true }, err => {
+  if (err) {
+    throw err;
+  }
+  clearFolder();
+  copy();
+});
 
+function copy() {
+  fs.readdir(path.join(__dirname, 'files'), 'utf-8', (err, files) => {
+    if (err) {
+      throw err;
+    }
+    files.forEach(file => {
+      fs.copyFile(path.join(__dirname, 'files', file), path.join(copyFolder, file), err => {
+        if (err) {
+          throw err;
+        }
+      });
+    });
+  });
+}
+function clearFolder() {
+  fs.readdir(copyFolder, 'utf-8', (err, files) => {
+    if (err) {
+      throw err;
+    }
+    files.forEach(file => {
+      fs.unlink(path.join(copyFolder, file), err => {
+        if (err) {
+          throw err;
+        }
+      });
+    });
+  });
+}
